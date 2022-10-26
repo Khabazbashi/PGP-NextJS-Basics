c8a022189ef421b5f3e55eba094acaa50cc1d51b
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-26 14:16:39 +0200

    Add component Navbar

---
+++ next-product-catalog/components/Navbar.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Filter.module.css";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>ADDAstore</h1>
+      <a>Home</a>
+      <a>Search</a>
+    </div>
+  );
+}
