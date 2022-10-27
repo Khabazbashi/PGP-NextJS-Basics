1d2ac0839782b35c0303b03a9cad18f89ff6545a
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-27 12:23:44 +0200

    Implement filter function

--- next-product-catalog/components/Filter.js
+++ next-product-catalog/components/Filter.js
@@ -1,27 +1,36 @@
 import { productsRepo } from "../helpers/productsrepo";
+import { useState } from "react";
+import Filterelement from "./Filterelement";
 import styles from "../styles/Filter.module.css";
 
 const Filter = ({ filter }) => {
   const categories = productsRepo.getCategories();
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+    var content = document.getElementById("content");
+
+    if (isActive) {
+      content.className = styles.dropdownContentActive;
+    } else {
+      content.className = styles.dropdownContent;
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.dropdown}>
-        <div className={styles.ul}>
-          <p className="dropbtn">Filter</p>
-          <div className={styles.dropdownContent}>
-            {categories.map((category, i) => {
-              return (
-                <button
-                  key={i}
-                  className={styles.li}
-                  onClick={() => filter(category)}
-                >
-                  {category}
-                </button>
-              );
-            })}
-          </div>
+        <button className={styles.dropbtn} onClick={() => handleClick()}>
+          CATEGORIES
+        </button>
+
+        <div id="content" className={styles.dropdownContent}>
+          {categories.map((category, i) => {
+            return (
+              <Filterelement key={i} filter={filter} category={category} />
+            );
+          })}
         </div>
       </div>
     </div>
--- next-product-catalog/helpers/productsrepo.js
+++ next-product-catalog/helpers/productsrepo.js
@@ -1,8 +1,27 @@
 let products = require("../data/data.json");
+let currentCategories = [];
 
 export const productsRepo = {
   getAll: () => products,
   getById: (id) => products.find((x) => x.id.toString() === id.toString()),
-  getByCategory: (category) => products.filter((x) => x.category === category),
   getCategories: () => Array.from(new Set(products.map((x) => x.category))),
+  getByCategory,
 };
+
+function getByCategory(category) {
+  if (currentCategories.includes(category)) {
+    var index = currentCategories.indexOf(category);
+    currentCategories.splice(index, 1);
+  } else {
+    currentCategories.push(category);
+  }
+
+  if (currentCategories.length === 0) {
+    return products;
+  }
+
+  const filteredProducts = products.filter((item) =>
+    currentCategories.includes(item.category)
+  );
+  return filteredProducts;
+}
